22d3e39db9fa63e14f554d41dda07322b164102c
Phạm Văn Dũng <dung18j@gmail.com>
2022-04-29 18:17:01 +0700

    HTTP's hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use askama::Template;
+use async_std::task::block_on;
+use tide::{Request, Response, Result};
+
+#[derive(Template, Debug)]
+#[template(path = "index.html")]
+struct IndexPage {
+    name: String,
+}
+
+async fn index_page(req: Request<()>) -> Result<Response> {
+    let res: Response = IndexPage {
+        name: req.param("name").unwrap_or("").to_owned(),
+    }
+    .into();
+    Ok(res)
+}
+
 fn main() {
-    println!("Hello, world!");
+    dotenv::dotenv().ok();
+
+    let mut server = tide::with_state(());
+
+    server.at("/").get(index_page);
+    server.at("/:name").get(index_page);
+
+    block_on(server.listen("0.0.0.0:8080")).unwrap();
 }
